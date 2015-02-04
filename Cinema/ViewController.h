a1ad762c77e6ee80d5c6c336e395504f04c480bf
manuntag <manuntag@ualberta.ca>
2015-02-04 11:46:58 -0800

    Initial Commit

---
+++ Cinema/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Cinema
+//
+//  Created by David Manuntag on 2015-02-04.
+//  Copyright (c) 2015 David Manuntag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
